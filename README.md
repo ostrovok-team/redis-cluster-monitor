6a7917c160cb5c3b27496f8b74b5e54aec982285
VadZay <vadzay@ostrovok.ru>
2013-02-05 20:21:52 +0400

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ redis-cluster-monitor
 =====================
 
 Redis cluster (Master-Slave replication) monitoring utility
+
+This is simple Ruby script for monitoring Redis clusters.
+It checks if all Redis servers are listening to the same master, if cluster has one and only one master.
+
+Usage:
+redistest.rb <serv1:port1> <serv2:port2> [serv3:port3] [serv4:port4]...
---
+++ redistest.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/ruby
+require "rubygems"
+require "redis"
+
+class ReplicatedRedis < Redis
+    def is_master?
+        self.info["role"] == "master"
+    end
+    def is_slave?
+        self.info["role"] == "slave"
+    end
+    def is_slave_of?(master)
+        raise ArgumentError unless master.is_a?(self.class)
+        unless self.is_slave?
+            return false
+        end
+        self.master == master.inspect
+    end
+    def master
+        return nil unless self.is_slave?
+        self.info["master_host"]+":"+self.info["master_port"].to_s
+    end
+    def inspect
+        "#{@client.host}:#{@client.port}"
+    end
+end
+
+def fail(str)
+    print str.chomp + "\n"
+    exit 1
+end
+
+redis_hosts = Array.new()
+ARGV.each do |arg|
+    next unless arg =~ /^"?([0-9.a-z]+):([0-9]+)"?$/
+    r = ReplicatedRedis.new({ :host => $1,
+                              :port => $2,
+                              :timeout => 0.5})
+    begin r.randomkey rescue next end
+    redis_hosts << r
+end
+
+fail "No available redis servers!" if redis_hosts.empty?
+# Find master/slave
+redis_masters = Array.new()
+redis_slaves = Array.new()
+redis_hosts.each { |redis_server|
+    if redis_server.is_master?
+        redis_masters << redis_server
+    elsif redis_server.is_slave?
+        redis_slaves << redis_server
+    else
+        fail "Unknown Redis role: #{redis_server.info["role"]}"
+    end
+}
+
+if redis_masters.length > 1
+    rms = Array.new
+    redis_masters.each do |rm|
+        rms << rm.inspect.to_s
+    end
+    fail "More than one master: "+rms.join(",")
+end
+fail "No Master!" if redis_masters.length < 1
+fail "No Slaves!" if redis_slaves.empty?
+
+redis_master = redis_masters[0]
+redis_slaves.each do |redis_slave|
+    unless redis_slave.is_slave_of?(redis_master)
+        fail "Redis #{redis_slave.inspect} is pointed to wrong master #{redis_slave.master}, should be #{redis_master.inspect}!"
+    end
+end
+
+keyname = "replicationtestkey"+Time.now.to_i.to_s
+redis_master[keyname] = keyname
+redis_slaves.each do |redis_server|
+    unless redis_server[keyname] == keyname
+        fail "Redis server #{redis_server.inspect} failed replication"
+    end
+end
+redis_master.del(keyname)
